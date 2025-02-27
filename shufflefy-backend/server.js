173a9fd1d9d2f0b30b104bde35803ae12ae9cc80
Daframp <blakeframps@icloud.com>
2025-02-27 22:28:27 +0000

    Database is fully connected, weights update, smart shuffle implemented

--- shufflefy-backend/server.js
+++ shufflefy-backend/server.js
@@ -33,7 +33,7 @@ const spotifyApi = new SpotifyWebApi({
 // Step 1: Get authorisation code
 app.get("/login", (req, res) => {
     console.log("login");
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI)}&scope=streaming user-read-email playlist-read-private user-read-private user-modify-playback-state&show_dialog=true`;
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI)}&scope=user-read-playback-state user-read-currently-playing streaming user-read-email playlist-read-private user-read-private user-modify-playback-state&show_dialog=false`;
     
     res.redirect(authUrl); // Redirect the user to the Spotify authorization page
 
@@ -104,9 +104,24 @@ app.get('/playlists', async (req, res) => {
 
 
 app.post('/api/play-track', async (req, res) => {
+    console.log("play track");
     const { trackUri , deviceId} = req.body;
 
     try {
+        try {
+        const currentTrackResponse = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: {
+                'Authorization': `Bearer ${req.session.accessToken}`,
+            },
+        });
+        const currentTrack = currentTrackResponse.data;
+
+        // If the track is already playing, return without doing anything
+        if (currentTrack && currentTrack.item.uri === trackUri) {
+            return res.json({ success: true, message: 'Track is already playing' });
+        }
+        }catch (error){
+        }
         const response = await axios.put(
             `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
             { uris: [trackUri] },
@@ -152,42 +167,57 @@ app.post('/api/add-to-queue', async (req, res) => {
     }
 });
 
-app.get('/api/get-random-song', async (req, res) => {
-    const { playlistId } = req.query;
+
+app.post('/api/get-songs', async (req, res) => {
+    const {playlistId} = req.body;
 
     if (!playlistId) {
         return res.status(400).json({ error: 'Playlist ID is required' });
     }
 
+    trackUris = [];
+    endOfPlaylist = false;
+    offset = 0;
+
+    //API gets only 100 songs at a time, so iterate until there is no link to next page
     try {
-        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-            headers: {
-                Authorization: `Bearer ${req.session.accessToken}`,
-            },
-        });
+        while (!endOfPlaylist){
+            const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${offset}`, {
+                headers: {
+                    Authorization: `Bearer ${req.session.accessToken}`,
+                },
+            });
 
-        if (!response.ok) {
-            return res.status(response.status).json({ error: 'Failed to fetch playlist tracks' });
-        }
+            if (!response.ok) {
+                return res.status(response.status).json({ error: 'Failed to fetch playlist tracks' });
+            }
 
-        const data = await response.json();
+            const data = await response.json();
 
-        if (!data.items || data.items.length === 0) {
-            return res.status(404).json({ error: 'No tracks found in the playlist' });
-        }
+            if (!data.items || data.items.length === 0) {
+                return res.status(404).json({ error: 'No tracks found in the playlist' });
+            }
+            const items = data.items;
+            for (const item of items){
+                trackUris.push(item.track.uri);
+            }
+            if (data.next == null){
+                endOfPlaylist = true;
+            }
+            offset += 100;
+        } 
 
-        const randomTrack = data.items[Math.floor(Math.random() * data.items.length)];
-        const trackUri = randomTrack.track.uri;
-
-        res.json({ trackUri: trackUri });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-});
+    
+
+        res.json({ trackUris: trackUris });
+
+})
 
 app.get("/api/getUserId", async (req, res) => {
-    console.error("Get userId");
     try {
         const response = await fetch("https://api.spotify.com/v1/me", {
             headers: {
@@ -210,6 +240,42 @@ app.get("/api/getUserId", async (req, res) => {
     }
 })
 
+app.post("/api/get-queue", async (req, res) => {
+    const {firstTrackUri} = req.body;
+    console.log("firstTrackUri: ", firstTrackUri);
+    console.log("auth token: ", req.session.accessToken);
+    try {
+        const response = await fetch("https://api.spotify.com/v1/me/player/queue", {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${req.session.accessToken}`
+            }
+        })
+
+        if (!response.ok) {
+            return res.status(response.status).json({ error: 'Failed to fetch queue' });
+        }
+
+        const data = await response.json();
+
+        const queue = data.queue;
+        const currentlyPlaying = data.currently_playing;
+
+        if (queue && queue.length > 1) {
+            const cleanedQueue = queue.filter((track) => {
+                console.log("trackID: ", track.id);
+                return track.uri !== firstTrackUri //Filter out spotify api bug of duping current song into queue
+            });
+            // console.log("cleaned queue: ", cleanedQueue);
+            res.json({ queue: cleanedQueue });
+        } else {
+            res.json({ queue: null });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 app.post("/db/getUserPlaylistId", async (req,res) => {
     const {userId, playlistId} = req.body;
 
@@ -220,7 +286,6 @@ app.post("/db/getUserPlaylistId", async (req,res) => {
              ON CONFLICT (UserID, PlaylistID) DO NOTHING;`,
             [userId, playlistId]
         );
-        console.log("Insert results: ", insertResult);
     } catch (error) {
         console.error('Error inserting UserPlaylist:', error);
         res.status(500).json({ error: 'Database error' });
@@ -247,42 +312,81 @@ app.post("/db/getUserPlaylistId", async (req,res) => {
 
 });
 
-app.post("/db/addSongs", async (req,res) => {
-    const {userId, playlistId} = req.body;
+app.post("/db/add-songs", async (req,res) => {
+    const {userPlaylistId, trackUris} = req.body;
 
-    try {
-        const insertResult =  await pool.query(
-            `INSERT INTO UserPlaylist (UserID, PlaylistID)
-             VALUES ($1, $2)
-             ON CONFLICT (UserID, PlaylistID) DO NOTHING;`,
-            [userId, playlistId]
-        );
-        console.log("Insert results: ", insertResult);
-    } catch (error) {
-        console.error('Error inserting UserPlaylist:', error);
-        res.status(500).json({ error: 'Database error' });
+    if (!trackUris || trackUris.length === 0) {
+        return res.status(400).json({ error: 'No songs provided' });
     }
 
+    const values = [];
+    const placeholders = trackUris.map((songId, index) => {
+        const weight = Math.floor(trackUris.length / 2); // Set the weight of new songs to half the size of the playlist
+        values.push(songId, userPlaylistId, weight);
+        return `($${index * 3 + 1}, $${index * 3 + 2}, $${index * 3 + 3})`; //Each record has parameters ($1, $2, $3), index adjusted
+    }).join(", ");
+    
+    const queryText = `
+        INSERT INTO songweight (SongId, UserPlaylistId, weight)
+        VALUES ${placeholders}
+        ON CONFLICT (SongID, UserPlaylistID) DO NOTHING;
+    `;
     try {
-        const queryText = `
-            SELECT UserPlaylistID
-            FROM UserPlaylist
-            WHERE UserID = $1 AND PlaylistID = $2;
-        `;
-        const result = await pool.query(queryText, [userId, playlistId]);
-        if (result.rows.length > 0) {
-            res.json({userPlaylistId: result.rows[0].userplaylistid});
-        } else {
-            res.status(400).json({error: 'No userPlaylistId found'})
-        }
-
-        
+        await pool.query(queryText, values);
+        res.json({ success: true });
     } catch (error) {
-        console.error('Error fetching UserPlaylistId:', error);
+        console.error('Error inserting songs into playlist:', error);
         res.status(500).json({ error: 'Database error' });
     }
 
 });
+
+app.post("/db/update-weights", async (req, res) => {
+    const { userPlaylistId, trackUri } = req.body;
+    console.log("userPlaylistId: ", userPlaylistId);
+    console.log("track Uri: ", trackUri);
+
+    try {
+        const queryText = `
+            UPDATE songweight
+            SET weight = CASE 
+                WHEN SongId = $1 THEN 0 
+                ELSE weight + 1 
+            END
+            WHERE UserPlaylistId = $2;
+        `;
+        
+        await pool.query(queryText, [trackUri, userPlaylistId]);
+        res.json({ success: true });
+    } catch (error) {
+        console.error('Error updating song weights:', error);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
+app.post("/db/get-song-weights", async (req,res) => {
+    const { userPlaylistId, songs } = req.body;
+
+    if (!userPlaylistId || !songs || songs.length === 0) {
+        return res.status(400).json({ error: "Missing required parameters" });
+    }
+
+    try {
+        const queryText = `
+            SELECT SongId, weight 
+            FROM songweight 
+            WHERE UserPlaylistId = $1 
+            AND SongId = ANY($2);
+        `;
+
+        const { rows } = await pool.query(queryText, [userPlaylistId, songs]);
+        res.json({ songWeights: rows });
+    } catch (error) {
+        console.error("Error fetching song weights:", error);
+        res.status(500).json({ error: "Database error" });
+    }
+})
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
