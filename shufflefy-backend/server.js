2709711c89d95fc2833dea5ccc35ec15dc6880ad
Daframp <blakeframps@icloud.com>
2025-05-15 14:38:55 +0100

    Use postgre session store to avoid memory leaks and give persistent sessions

--- shufflefy-backend/server.js
+++ shufflefy-backend/server.js
@@ -1,20 +1,23 @@
-const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const SpotifyWebApi = require("spotify-web-api-node");
-const session = require("express-session");
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
 const path = require("path");
 const pool = require('./db'); 
 
 require('dotenv').config();
 
-const redisClient = redis.createClient();
+const express = require("express");
+const session = require("express-session");
+
+const PgSession = require('connect-pg-simple')(session);
+
 
 const app = express();
 app.use(session({
-    store: new RedisStore({ client: redisClient }),
+    store: new PgSession({
+        pool: pool,                // Connection pool
+        tableName: 'session'
+    }),
     secret: process.env.SESSION_SECRET || 'default_secret', // Use an environment variable or default secret
     resave: false,
     saveUninitialized: true,
